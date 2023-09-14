39bf6cfaf7382c02c92c26451c845fb6efc251ad
Щукин Евгений Владимирович <tavda_hak@mail.ru>
2023-09-14 07:52:47 +0500

    Добавил реализующий задание класс

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+public static class server
+{
+    static object sync = new object();
+    private static int count;
+
+    public static int GetCount()
+    {
+        Monitor.Wait(sync);
+        return count;
+    }
+
+    public static void AddToCount(int value)
+    {
+        lock (sync)
+        {
+            count += value;
+        }
+    }
+
+}
