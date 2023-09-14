873c4a373b04ec91f1cd6072591bc3761f81aaa5
Щукин Евгений Владимирович <tavda_hak@mail.ru>
2023-09-14 10:21:26 +0500

    Видимо вот так безопасно

--- Program.cs
+++ Program.cs
@@ -1,21 +1,45 @@
-﻿
-public static class server
+﻿for (int i = 1; i < 6; i++)
 {
-    static object sync = new object();
+    Thread myThread = new(() =>
+    {
+        var numb = i;
+        while (true)
+        {
+            Thread.Sleep(100);
+            Console.WriteLine($"Поток Чтения {numb}" + " GetCount: [" + Server.GetCount() + "]");
+        }
+    });
+    myThread.Start();
+}
+
+for (int i = 1; i < 6; i++)
+{
+    Thread myThread = new(() =>
+    {
+        var numb = i;
+        while (true)
+        {
+            
+            Server.AddToCount(numb);
+            Console.WriteLine($"Поток Записи {numb}");
+        }
+    });
+    myThread.Start();
+}
+
+Console.ReadLine();
+
+public static class Server
+{
     private static int count;
 
     public static int GetCount()
     {
-        Monitor.Wait(sync);
-        return count;
+        return Interlocked.CompareExchange(ref count, 0, 0);
     }
 
     public static void AddToCount(int value)
     {
-        lock (sync)
-        {
-            count += value;
-        }
+        Interlocked.Add(ref count, value);
     }
-
 }
